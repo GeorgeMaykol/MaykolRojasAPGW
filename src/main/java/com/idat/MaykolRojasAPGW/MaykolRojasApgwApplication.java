d0293e50be5fd13979865a3e89db8204819f2ac4
Maykol Rojas <maykolroj@gmail.com>
2022-06-04 07:38:24 -0500

    API Gateway Terminado

---
+++ src/main/java/com/idat/MaykolRojasAPGW/MaykolRojasApgwApplication.java
@@ -0,0 +1,13 @@
+package com.idat.MaykolRojasAPGW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaykolRojasApgwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaykolRojasApgwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/idat/MaykolRojasAPGW/MaykolRojasApgwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idat.MaykolRojasAPGW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaykolRojasApgwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
